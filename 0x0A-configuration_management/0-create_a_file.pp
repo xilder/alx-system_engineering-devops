1d3304ee53627104cfd1fdafea54074faf13b5b3
xilder <olabowaleayobami@gmail.com>
2023-11-24 10:44:57 -0800

    format 0-create_a_file.pp properly

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file { '/tmp/school':
 #  path    => '/tmp/school',
   ensure  => file,
-  owner    => 'www-data',
-  group    => 'www-data',
+  owner   => 'www-data',
+  group   => 'www-data',
   mode    => '0744',
-  content  => 'I love Puppet'
+  content => 'I love Puppet'
   }

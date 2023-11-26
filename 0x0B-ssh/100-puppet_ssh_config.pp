3b656eb219e20816ad9741b6b129f38a0622b599
xilder <olabowaleayobami@gmail.com>
2023-11-26 14:15:09 -0800

    up date  100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,16 @@
+# Setting up the client config file
 include stdlib
 
-file_line { 'change identity_file':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '	IdentityFile ~/.ssh/school',
- replace => true,
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'turn off pwd auth':
-  ensure   => present,
-  path     => '/etc/ssh/ssh_config',
-  line     => '	PasswordAuthentication no',
-   replace => true,
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }

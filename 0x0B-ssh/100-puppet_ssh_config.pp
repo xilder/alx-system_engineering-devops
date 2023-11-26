5525fbe99d7c1a443fbba00e628696e236e55c37
xilder <olabowaleayobami@gmail.com>
2023-11-26 08:00:40 -0800

    adds 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+file_line { 'change identity_file':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config',
+  line   => '	IdentityFile ~/.ssh/school',
+}
+
+file_line { 'turn off pwd auth':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config',
+  line   => '	IdentityFile ~/.ssh/school',
+}

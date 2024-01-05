25eae718c9dd4b31af36243a55467b29ff9b01e0
xilder <olabowaleayobami@gmail.com>
2024-01-05 10:26:38 -0800

    updates

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,6 +5,7 @@ for a given employee ID, returns information about his/her TODO list progress
 from sys import argv
 import requests
 
+
 def get_todo_list():
     base_url = "https://jsonplaceholder.typicode.com"
     user_endpoint = f"users/{argv[1]}"
@@ -14,11 +15,12 @@ def get_todo_list():
     user_id = argv[1]
     user = requests.get(user_url).json()
     todos = requests.get(user_todo_url).json()
-    
+
     with open(f"{user_id}.csv", "w") as f:
         for todo in todos:
             f.write(f'"{user_id}","{user["username"]}",' +
                     f'"{todo["completed"]}","{todo["title"]}"\n')
+
 
 if __name__ == "__main__":
     get_todo_list()

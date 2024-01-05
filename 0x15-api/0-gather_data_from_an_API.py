50db2b14ec122de7e3dca9837ee086f86ac5f588
xilder <olabowaleayobami@gmail.com>
2024-01-04 20:02:07 -0800

    adds 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+for a given employee ID, returns information about his/her TODO list progress
+"""
+from sys import argv
+import requests
+
+def get_todo_list():
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_endpoint = f"users/{argv[1]}"
+    todo_endpoint = f"todos"
+    user_url = f"{base_url}/{user_endpoint}"
+    user_todo_url = f"{user_url}/{todo_endpoint}"
+
+    user = requests.get(user_url).json()
+    todos = requests.get(user_todo_url).json()
+    done = [x for x in todos if x["completed"] is True]
+    print(f"Employee {user['name']} is done with " + 
+           f"tasks({len(done)}/{len(todos)})")
+    for todo in done:
+        print(f"\t{todo['title']}")
+
+
+if __name__ == "__main__":
+    get_todo_list()

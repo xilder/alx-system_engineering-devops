379aa5be8e7d6b717df6c0ea3990801d14a683fa
xilder <olabowaleayobami@gmail.com>
2024-01-16 16:13:09 -0800

    adds 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+gets the total number of subscribers
+"""
+from requests import get
+
+def number_of_subscribers(subreddit):
+    """
+    gets the total number of subscribers
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+ALX - 0x16-api_advanced
